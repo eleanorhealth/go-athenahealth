7b74513b7336d6589488d8789be9dce7c9b3ddf4
Frank Terragna <fterragna@gmail.com>
2020-06-02 17:17:18 -0400

    Added tests for HTTPClient, GetPatient, and ListPatients (#1)

---
+++ athenahealth/patients_test.go
@@ -0,0 +1,51 @@
+package athenahealth
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPClient_GetPatient(t *testing.T) {
+	assert := assert.New(t)
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		b, _ := ioutil.ReadFile("./resources/GetPatient.json")
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	patient, err := athenaClient.GetPatient("1")
+
+	assert.NotNil(patient)
+	assert.Nil(err)
+}
+
+func TestHTTPClient_ListPatients(t *testing.T) {
+	assert := assert.New(t)
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal("John", r.URL.Query().Get("firstname"))
+		assert.Equal("Smith", r.URL.Query().Get("lastname"))
+
+		b, _ := ioutil.ReadFile("./resources/ListPatients.json")
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	opts := &ListPatientsOptions{
+		FirstName: "John",
+		LastName:  "Smith",
+	}
+
+	patients, err := athenaClient.ListPatients(opts)
+
+	assert.Len(patients, 2)
+	assert.Nil(err)
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/go-redis/redis/v8 v8.0.0-beta.2
+	github.com/stretchr/testify v1.6.0
 )
