6e627510b4cc1f4e782c625f104e22d13b5992ec
Frank Terragna <fterragna@gmail.com>
2020-10-14 15:11:41 -0400

    Added ListChangedPatients and ListChangedProviders (#23)

--- athenahealth/patients_test.go
+++ athenahealth/patients_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,6 +15,8 @@ func TestHTTPClient_GetPatient(t *testing.T) {
 
 	h := func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal("true", r.URL.Query().Get("showcustomfields"))
+		assert.Equal("true", r.URL.Query().Get("showinsurance"))
+		assert.Equal("true", r.URL.Query().Get("showportalstatus"))
 
 		b, _ := ioutil.ReadFile("./resources/GetPatient.json")
 		w.Write(b)
@@ -25,6 +28,8 @@ func TestHTTPClient_GetPatient(t *testing.T) {
 	id := "1"
 	opts := &GetPatientOptions{
 		ShowCustomFields: true,
+		ShowInsurance:    true,
+		ShowPortalStatus: true,
 	}
 	patient, err := athenaClient.GetPatient(id, opts)
 
@@ -91,5 +96,40 @@ func TestHTTPClient_UpdatePatientPhoto(t *testing.T) {
 
 	id := "1"
 	err := athenaClient.UpdatePatientPhoto(id, data)
+	assert.NoError(err)
+}
+
+func TestHTTPClient_ListChangedPatients(t *testing.T) {
+	assert := assert.New(t)
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal("d1", r.URL.Query().Get("departmentid"))
+		assert.Equal("true", r.URL.Query().Get("ignorerestrictions"))
+		assert.Equal("true", r.URL.Query().Get("leaveunprocessed"))
+		assert.Equal("p1", r.URL.Query().Get("patientid"))
+		assert.Equal("true", r.URL.Query().Get("returnglobalid"))
+		assert.Equal("06/01/2020 15:30:45", r.URL.Query().Get("showprocessedstartdatetime"))
+		assert.Equal("06/02/2020 12:30:45", r.URL.Query().Get("showprocessedenddatetime"))
+
+		b, _ := ioutil.ReadFile("./resources/ListChangedPatients.json")
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	opts := &ListChangedPatientOptions{
+		DepartmentID:               "d1",
+		IgnoreRestrictions:         true,
+		LeaveUnprocessed:           true,
+		PatientID:                  "p1",
+		ReturnGlobalID:             true,
+		ShowProcessedStartDatetime: time.Date(2020, 6, 1, 15, 30, 45, 0, time.UTC),
+		ShowProcessedEndDatetime:   time.Date(2020, 6, 2, 12, 30, 45, 0, time.UTC),
+	}
+
+	patients, err := athenaClient.ListChangedPatients(opts)
+
+	assert.Len(patients, 1)
 	assert.NoError(err)
 }
