8213037db044b232bf636d2e5ac9f0e5f244ae85
Frank Terragna <fterragna@gmail.com>
2021-03-11 01:18:05 -0500

    NewDatadog now accepts a statsd.ClientInterface

--- athenahealth/stats/datadog.go
+++ athenahealth/stats/datadog.go
@@ -1,51 +1,34 @@
 package stats
 
 import (
-	"fmt"
-
 	"github.com/DataDog/datadog-go/statsd"
 )
 
 const datadogDefaultNamespace = "athenahealth-client"
 
 type Datadog struct {
-	statsClient *statsd.Client
+	client statsd.ClientInterface
 }
 
-func NewDatadog(addr, namespace string) (*Datadog, error) {
-	if len(addr) == 0 {
-		addr = ":8125"
-	}
-
-	if len(namespace) == 0 {
-		namespace = datadogDefaultNamespace
-	}
-
-	statsClient, err := statsd.New(addr)
-	if err != nil {
-		return nil, err
-	}
-
-	statsClient.Namespace = fmt.Sprintf("%s.", namespace)
-
+func NewDatadog(client statsd.ClientInterface) *Datadog {
 	return &Datadog{
-		statsClient: statsClient,
-	}, nil
+		client: client,
+	}
 }
 
 func (d *Datadog) Request() error {
-	err := d.statsClient.Count("requests.count", 1, []string{}, 1.0)
+	err := d.client.Count("athenahealth.requests.count", 1, []string{}, 1.0)
 	if err != nil {
 		return err
 	}
 
-	return d.statsClient.Incr("requests", []string{}, 1.0)
+	return d.client.Incr("athenahealth.requests", []string{}, 1.0)
 }
 
 func (d *Datadog) ResponseSuccess() error {
-	return d.statsClient.Incr("responses.success", []string{}, 1.0)
+	return d.client.Incr("athenahealth.responses.success", []string{}, 1.0)
 }
 
 func (d *Datadog) ResponseError() error {
-	return d.statsClient.Incr("responses.error", []string{}, 1.0)
+	return d.client.Incr("responses.error", []string{}, 1.0)
 }
