123032b8e82ad59d8e199bc03c4895c17a15e184
Ellie Hastings <ellie.hastings@eleanorhealth.com>
2023-03-21 15:56:33 -0400

    Add ListMedications method (#78)

--- athenahealth/medications.go
+++ athenahealth/medications.go
@@ -2,19 +2,100 @@ package athenahealth
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 )
 
+type MedicationEventType string
+
+const (
+	EventTypeStart MedicationEventType = "START"
+	EventTypeEnd   MedicationEventType = "END"
+	EventTypeOrder MedicationEventType = "ORDER"
+	EventTypeEnter MedicationEventType = "ENTER"
+	EventTypeFill  MedicationEventType = "FILL"
+	EventTypeHide  MedicationEventType = "HIDE"
+)
+
+type MedicationEvent struct {
+	EventDate       string              `json:"eventdate"`
+	Type            MedicationEventType `json:"type"`
+	UserDisplayName string              `json:"userdisplayname,omitempty"`
+}
+
+type MedicationStructuredSig struct {
+	DosageAdditionalInstructions string       `json:"dosageadditionalinstructions,omitempty"`
+	DosageFrequencyValue         NumberString `json:"dosagefrequencyvalue,omitempty"`
+	DosageRoute                  string       `json:"dosageroute"`
+	DosageAction                 string       `json:"dosageaction"`
+	DosageFrequencyUnit          string       `json:"dosagefrequencyunit"`
+	DosageQuantityUnit           string       `json:"dosagequantityunit"`
+	DosageQuantityValue          NumberString `json:"dosagequantityvalue"`
+	DosageFrequencyDescription   string       `json:"dosagefrequencydescription"`
+	DosageDurationUnit           string       `json:"dosagedurationunit"`
+	DosageDurationValue          NumberString `json:"dosagedurationvalue"`
+}
+
 type Medication struct {
+	ClinicalOrderTypeID NumberString            `json:"clinicalordertypeid"`
+	CreatedBy           string                  `json:"createdby"`
+	Events              []MedicationEvent       `json:"events"`
+	FDBMedicationID     NumberString            `json:"fdbmedicationid"`
+	IsDiscontinued      bool                    `json:"isdiscontinued"`
+	IsSafeToRenew       bool                    `json:"issafetorenew"`
+	IsStructuredSig     bool                    `json:"isstructuredsig"`
+	LastModifiedBy      string                  `json:"lastmodifiedby,omitempty"`
+	LastModifiedDate    string                  `json:"lastmodifieddate,omitempty"`
+	Medication          string                  `json:"medication"`
+	MedicationEntryID   string                  `json:"medicationentryid"`
+	MedicationID        NumberString            `json:"medicationid"`
+	OrganClass          string                  `json:"organclass"`
+	Source              string                  `json:"source"`
+	StructuredSig       MedicationStructuredSig `json:"structuredsig,omitempty"`
+	UnstructuredSig     string                  `json:"unstructuredsig,omitempty"`
+}
+
+type ListMedicationsResult struct {
+	LastUpdated                 string         `json:"lastupdated"`
+	Medications                 [][]Medication `json:"medications"`
+	NoMedicationsReported       bool           `json:"nomedicationsreported"`
+	PatientDownloadConsent      bool           `json:"patientdownloadconsent"`
+	PatientNeedsDownloadConsent bool           `json:"patientneedsdownloadconsent"`
+}
+
+type ListMedicationsOptions struct {
+	DepartmentID string
+}
+
+type SearchMedicationsResult struct {
 	Medication   string `json:"medication"`
 	MedicationID int    `json:"medicationid"`
+}
+
+// ListMedications - Retrieves a list of medications for a given patient
+// GET /v1/{practiceid}/chart/{patientid}/medications
+// https://docs.athenahealth.com/api/api-ref/medication#Get-patient's-medication-list
+func (h *HTTPClient) ListMedications(ctx context.Context, patientID string, opts *ListMedicationsOptions) (*ListMedicationsResult, error) {
+	out := &ListMedicationsResult{}
+
+	q := url.Values{}
+	if len(opts.DepartmentID) > 0 {
+		q.Add("departmentid", opts.DepartmentID)
+	}
+
+	_, err := h.Get(ctx, fmt.Sprintf("/chart/%s/medications", patientID), q, &out)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
 }
 
 // SearchMedications - Retrieves a list of medications for a given search parameters.
 // GET /v1/{practiceid}/reference/medications
 // https://docs.athenahealth.com/api/api-ref/medication#Search-for-available-medications
-func (h *HTTPClient) SearchMedications(ctx context.Context, searchVal string) ([]*Medication, error) {
-	out := []*Medication{}
+func (h *HTTPClient) SearchMedications(ctx context.Context, searchVal string) ([]*SearchMedicationsResult, error) {
+	out := []*SearchMedicationsResult{}
 
 	q := url.Values{}
 	q.Add("searchvalue", searchVal)
--- athenahealth/medications_test.go
+++ athenahealth/medications_test.go
@@ -9,6 +9,55 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestHTTPClient_ListMedications(t *testing.T) {
+	assert := assert.New(t)
+
+	patientID := "123"
+	departmentID := "789"
+	opts := &ListMedicationsOptions{DepartmentID: departmentID}
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(departmentID, r.URL.Query().Get("departmentid"))
+
+		b, _ := os.ReadFile("./resources/ListMedications.json")
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	medicationsResult, err := athenaClient.ListMedications(context.Background(), patientID, opts)
+
+	assert.NotNil(medicationsResult)
+	assert.Len(medicationsResult.Medications, 5)
+	assert.NoError(err)
+}
+
+func TestHTTPClient_ListMedications_None(t *testing.T) {
+	assert := assert.New(t)
+
+	patientID := "123"
+	departmentID := "789"
+	opts := &ListMedicationsOptions{DepartmentID: departmentID}
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(departmentID, r.URL.Query().Get("departmentid"))
+
+		b := []byte(`{
+			"patientneedsdownloadconsent": false,"medications": [],"patientdownloadconsent": true,"nomedicationsreported": false}`)
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	medicationsResult, err := athenaClient.ListMedications(context.Background(), patientID, opts)
+
+	assert.NotNil(medicationsResult)
+	assert.Len(medicationsResult.Medications, 0)
+	assert.NoError(err)
+}
+
 func TestHTTPClient_SearchMedications(t *testing.T) {
 	assert := assert.New(t)
 
