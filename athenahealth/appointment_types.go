46ac89ab9984b07a945c28bf9e9f8e264fbd9018
Sean T Canavan <githubprivate@hisean.net>
2023-06-13 15:24:27 -0500

    1 of 2: Add support for Create Appointment Type (#96)

---
+++ athenahealth/appointment_types.go
@@ -0,0 +1,53 @@
+package athenahealth
+
+import (
+	"context"
+	"net/url"
+	"strconv"
+)
+
+type CreateAppointmentTypeOptions struct {
+	Duration         string `json:"duration"`
+	Generic          *bool  `json:"generic"`
+	Name             string `json:"name"`
+	Patient          bool   `json:"patient"`
+	ShortName        string `json:"shortname"`
+	TemplateTypeOnly *bool  `json:"templatetypeonly"`
+}
+
+type CreateAppointmentTypeResult struct {
+	AppointmentTypeID int `json:"appointmenttypeid"`
+}
+
+// CreateAppointmentType creates an Appointment Type
+// POST /v1/{practiceid}/appointmenttypes
+// https://docs.athenahealth.com/api/api-ref/appointment-types
+func (h *HTTPClient) CreateAppointmentType(ctx context.Context, opts *CreateAppointmentTypeOptions) (*CreateAppointmentTypeResult, error) {
+	out := CreateAppointmentTypeResult{}
+
+	q := url.Values{}
+	if opts != nil {
+		if opts.Duration != "" {
+			q.Set("duration", opts.Duration)
+		}
+		if opts.Name != "" {
+			q.Set("name", opts.Name)
+		}
+		if opts.ShortName != "" {
+			q.Set("shortname", opts.ShortName)
+		}
+		if opts.Generic != nil && *opts.Generic {
+			q.Set("generic", strconv.FormatBool(*opts.Generic))
+		}
+		if opts.Patient {
+			q.Set("patient", strconv.FormatBool(opts.Patient))
+		}
+		if opts.TemplateTypeOnly != nil && *opts.TemplateTypeOnly {
+			q.Set("templatetypeonly", strconv.FormatBool(*opts.TemplateTypeOnly))
+		}
+	}
+
+	_, err := h.PostForm(ctx, "/appointmenttypes", q, &out)
+
+	return &out, err
+}
---
+++ athenahealth/appointment_types_test.go
@@ -0,0 +1,46 @@
+package athenahealth
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestHTTPClient_CreateAppointmentType(t *testing.T) {
+	assert := assert.New(t)
+
+	opts := &CreateAppointmentTypeOptions{
+		Duration:         "99",
+		Generic:          func() *bool { a := true; return &a }(),
+		Name:             "1001",
+		Patient:          true,
+		ShortName:        "11",
+		TemplateTypeOnly: func() *bool { a := true; return &a }(),
+	}
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		assert.NoError(r.ParseForm())
+
+		assert.Equal(r.Form.Get("duration"), opts.Duration)
+		assert.Equal(r.Form.Get("generic"), strconv.FormatBool(*opts.Generic))
+		assert.Equal(r.Form.Get("name"), opts.Name)
+		assert.Equal(r.Form.Get("patient"), strconv.FormatBool(opts.Patient))
+		assert.Equal(r.Form.Get("shortname"), opts.ShortName)
+		assert.Equal(r.Form.Get("templatetypeonly"), strconv.FormatBool(*opts.TemplateTypeOnly))
+		assert.Equal(r.URL.Path, "/appointmenttypes")
+		b, _ := os.ReadFile("./resources/CreateAppointmentType.json")
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	createAppointmentTypeResult, err := athenaClient.CreateAppointmentType(context.Background(), opts)
+
+	assert.NotNil(createAppointmentTypeResult)
+	assert.NoError(err)
+	assert.Equal(5, createAppointmentTypeResult.AppointmentTypeID)
+}
