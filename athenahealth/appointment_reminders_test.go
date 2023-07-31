a18e6fd6e45b9f8c85d8cfd0208aecbebabc4f27
Adam Perkins <aspsc2@gmail.com>
2023-07-31 15:38:26 -0400

    List appt reminders (#113)
    * Add list appointment reminders
    
    * Add result fields/assertions
    
    * Add trailing space
    
    * PR feedback

---
+++ athenahealth/appointment_reminders_test.go
@@ -0,0 +1,61 @@
+package athenahealth
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestHTTPClient_ListAppointmentReminders(t *testing.T) {
+	assert := assert.New(t)
+
+	appointmentTypeID := 123
+	patientID := 456
+	providerID := 789
+	showDeleted := false
+
+	opts := &ListAppointmentRemindersOptions{
+		StartDate:         time.Date(2011, 9, 22, 0, 0, 0, 0, time.UTC),
+		EndDate:           time.Date(2022, 2, 25, 0, 0, 0, 0, time.UTC),
+		DepartmentID:      "1",
+		AppointmentTypeID: &appointmentTypeID,
+		PatientID:         &patientID,
+		ProviderID:        &providerID,
+		ShowDeleted:       &showDeleted,
+	}
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		assert.Equal("09/22/2011", query.Get("startdate"))
+		assert.Equal("02/25/2022", query.Get("enddate"))
+		assert.Equal(opts.DepartmentID, query.Get("departmentid"))
+
+		assert.Equal("123", query.Get("appointmenttypeid"))
+		assert.Equal("456", query.Get("patientid"))
+		assert.Equal("789", query.Get("providerid"))
+		assert.Equal("false", query.Get("showdeleted"))
+
+		b, _ := os.ReadFile("./resources/ListAppointmentReminders.json")
+		w.Write(b)
+	}
+
+	athenaClient, ts := testClient(h)
+	defer ts.Close()
+
+	apppointmentRemindersResult, err := athenaClient.ListAppointmentReminders(context.Background(), opts)
+	assert.NoError(err)
+	assert.Len(apppointmentRemindersResult.Reminders, 2)
+	ar := apppointmentRemindersResult.Reminders[0]
+	assert.Equal(52601, ar.AppointmentReminderID)
+	assert.Equal(2398, ar.PatientID)
+	assert.Equal(5, ar.DepartmentID)
+	assert.Equal(342, ar.AppointmentTypeID)
+	assert.Equal(23, ar.ProviderID)
+	assert.Equal("Misc test notes", ar.Note)
+	assert.Equal("patient insttructionsss", ar.PatientInstructions)
+	assert.Equal("", ar.StaffInstructions)
+
+}
