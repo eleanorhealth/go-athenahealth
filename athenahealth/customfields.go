8f1e29161a62b4fbf293c8f43485d71bd26c0fbb
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2025-01-30 14:02:20 -0300

    feat(athenahealth/customfields): add new response fields (#142)
    * feat(athenahealth/customfields): add new response fields
    
    * misc: go mod tidy

--- athenahealth/customfields.go
+++ athenahealth/customfields.go
@@ -8,14 +8,22 @@ type CustomFieldValue struct {
 	OptionID         string `json:"optionid"`
 }
 
+type SelectOption struct {
+	OptionID    string `json:"optionid"`
+	OptionValue string `json:"optionvalue"`
+}
+
 type CustomField struct {
-	CaseSensitive  bool   `json:"casesensitive"`
-	CustomFieldID  string `json:"customfieldid"`
-	DisallowUpdate bool   `json:"disallowupdate"`
-	Name           string `json:"name"`
-	Searchable     bool   `json:"searchable"`
-	Select         bool   `json:"select"`
-	Type           string `json:"type"`
+	CaseSensitive  bool            `json:"casesensitive"`
+	CustomFieldID  string          `json:"customfieldid"`
+	DisallowUpdate bool            `json:"disallowupdate"`
+	MaxLength      string          `json:"length"`
+	Name           string          `json:"name"`
+	Ordering       string          `json:"ordering"`
+	Searchable     bool            `json:"searchable"`
+	SelectList     []*SelectOption `json:"selectlist"`
+	Select         bool            `json:"select"`
+	Type           string          `json:"type"`
 }
 
 // ListCustomFields - List of custom fields for the practice
